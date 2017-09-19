828af0ecfbc14c294937273486af404aa047bb92
Ronan Giron <ElGigi@users.noreply.github.com>
2017-09-19 18:07:54 +0200

    New type 'Number'

---
+++ src/Type/Number.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * This file is part of Berlioz framework.
+ *
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @copyright 2017 Ronan GIRON
+ * @author    Ronan GIRON <https://github.com/ElGigi>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code, to the root.
+ */
+
+namespace Berlioz\Form\Type;
+
+use Berlioz\Form\FormValidation;
+
+class Number extends Text
+{
+    const TYPE = 'number';
+
+    /**
+     * Email constructor.
+     *
+     * @param string $name    Name
+     * @param array  $options Options
+     */
+    public function __construct(string $name, array $options = [])
+    {
+        parent::__construct($name, $options);
+
+        // Validation
+        $this->addValidation(new FormValidation([$this, 'validation']));
+    }
+
+    /**
+     * Validation.
+     *
+     * @return bool
+     */
+    public function validation()
+    {
+        return preg_match('/^[0-9]+(\.[0-9]+)?$/', $this->getValue()) == 1;
+    }
+}
