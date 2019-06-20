cc87a3e269d3a5107219693ef432e8d03ee8c10f
Ronan Giron <ElGigi@users.noreply.github.com>
2019-06-20 18:48:54 +0200

    Add default transformer

---
+++ src/Transformer/DefaultTransformer.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * This file is part of Berlioz framework.
+ *
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @copyright 2017 Ronan GIRON
+ * @author    Ronan GIRON <https://github.com/ElGigi>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code, to the root.
+ */
+
+namespace Berlioz\Form\Transformer;
+
+use Berlioz\Form\Transformer;
+
+class DefaultTransformer implements Transformer
+{
+    /**
+     * Transform data to form.
+     *
+     * @param mixed $data
+     *
+     * @return mixed
+     */
+    public function toForm($data)
+    {
+        return $data;
+    }
+
+    /**
+     * Transform data from form.
+     *
+     * @param mixed $data
+     *
+     * @return mixed
+     */
+    public function fromForm($data)
+    {
+        return $data;
+    }
+}
