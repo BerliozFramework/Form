2d6762b771bf3de9c3a8db134550b1970949ed29
Ronan Giron <ElGigi@users.noreply.github.com>
2019-09-18 17:40:57 +0200

    Add AbstractCompositeType class

---
+++ src/Type/AbstractCompositeType.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * This file is part of Berlioz framework.
+ *
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @copyright 2017 Ronan GIRON
+ * @author    Ronan GIRON <https://github.com/ElGigi>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code, to the root.
+ */
+
+namespace Berlioz\Form\Type;
+
+use Berlioz\Form\Element\AbstractElement;
+use Berlioz\Form\Group;
+
+/**
+ * Class AbstractCompositeType.
+ *
+ * @package Berlioz\Form\Type
+ */
+abstract class AbstractCompositeType extends Group implements TypeInterface
+{
+    /////////////
+    /// VALUE ///
+    /////////////
+
+    /**
+     * @inheritdoc
+     */
+    public function getFinalValue()
+    {
+        return AbstractElement::getFinalValue();
+//        return $this->getTransformer()->fromForm($this->getValue(), $this);
+    }
+}
